2799a31f4004185bb4cd8ee25e9ba32d766975cd
kphutiyagae <kopanophutiyagae@gmail.com>
2023-04-03 23:14:17 +0200

    feat(none): :sparkles: Added card-stack-container component.
    * Added card stack container that will contain swipeable cards on dashboard.

---
+++ libs/app/dashboard/feature/src/lib/card-stack-container/card-stack-container.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-stack-container',
+  templateUrl: './card-stack-container.component.html',
+  styleUrls: ['./card-stack-container.component.scss'],
+})
+export class CardStackContainerComponent {}
