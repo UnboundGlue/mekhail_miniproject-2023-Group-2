2e9d94d35000476253e854b7e77792811a186421
kphutiyagae <kopanophutiyagae@gmail.com>
2023-04-11 23:00:00 +0200

    feat(none): :construction: WIP: Added custom card component and buttons to card-container component.

--- libs/app/feed/feature/src/lib/app-feed-feature.module.ts
+++ libs/app/feed/feature/src/lib/app-feed-feature.module.ts
@@ -5,6 +5,7 @@ import { IonicModule } from '@ionic/angular';
 import { appFeedFeatureRoutes } from './lib.routes';
 import { FeedPageComponent } from './feed-page/feed-page.component';
 import { CardStackContainerComponent } from './card-stack-container/card-stack-container.component';
+import { CardComponentComponent } from './card-component/card-component.component';
 
 @NgModule({
   imports: [
@@ -12,6 +13,10 @@ import { CardStackContainerComponent } from './card-stack-container/card-stack-c
     RouterModule.forChild(appFeedFeatureRoutes),
     IonicModule,
   ],
-  declarations: [FeedPageComponent, CardStackContainerComponent],
+  declarations: [
+    FeedPageComponent,
+    CardStackContainerComponent,
+    CardComponentComponent,
+  ],
 })
 export class AppFeedFeatureModule {}
--- libs/app/feed/feature/src/lib/card-stack-container/card-stack-container.component.ts
+++ libs/app/feed/feature/src/lib/card-stack-container/card-stack-container.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+import {IUser} from "@mp/api/users/util";
 
 @Component({
   selector: 'mp-card-stack-container',
   templateUrl: './card-stack-container.component.html',
   styleUrls: ['./card-stack-container.component.scss'],
 })
-export class CardStackContainerComponent {}
+export class CardStackContainerComponent {
+
+  userList$ = new Observable<IUser>;  //Convert to state selector next
+
+
+
+}
