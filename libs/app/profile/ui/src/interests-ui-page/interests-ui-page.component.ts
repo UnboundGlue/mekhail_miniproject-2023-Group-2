21375b37b78d78d9989c03913ffac84dab708e05
Klairgo <olwagekarel@gmail.com>
2023-04-12 17:29:57 +0200

    Deleted profile components that is not  needed

--- libs/app/profile/ui/src/interests-ui-page/interests-ui-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'interests-ui-page',
-  templateUrl: './interests-ui-page.component.html',
-  styleUrls: ['./interests-ui-page.component.scss'],
-})
-export class InterestsUiPageComponent {}
