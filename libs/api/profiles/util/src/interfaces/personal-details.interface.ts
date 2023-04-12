719f321d8f936ee0436124d0c763f7f518486d65
Klairgo <olwagekarel@gmail.com>
2023-04-12 14:58:00 +0200

    Updated profile interface

--- libs/api/profiles/util/src/interfaces/personal-details.interface.ts
+++ libs/api/profiles/util/src/interfaces/personal-details.interface.ts
@@ -1,8 +1,4 @@
-import { AgeGroup, Ethnicity, Gender, ProfileStatus } from '../enums';
-
 export interface IPersonalDetails {
-  age?: AgeGroup | null | undefined;
-  gender?: Gender | null | undefined;
-  ethnicity?: Ethnicity | null | undefined;
-  status?: ProfileStatus | null | undefined;
+  Firstname?: string;
+  Lastname?: string | null| undefined;
 }
