32c18c9f3cc8a7d605785c839806425e86a284f1
GT-GillesTeuwen <gillesteuwen@gmail.com>
2023-04-15 15:28:53 +0200

    Upload profile photo (not linked to profile yet)

--- libs/app/profile/feature/src/profile.page.ts
+++ libs/app/profile/feature/src/profile.page.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IProfile } from '@mp/api/profiles/util';
 import { ProfileState } from '@mp/app/profile/data-access';
 import { Select, Store } from '@ngxs/store';
@@ -8,7 +8,9 @@ import { OverlayEventDetail } from '@ionic/core/components';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActionsExecuting, actionsExecuting } from '@ngxs-labs/actions-executing';
 import { SaveProfileChanges } from '../../util/src/profile.actions';
-import { Logout } from '@mp/app/auth/util';
+import { Logout } from '@mp/app/auth/util'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+
 
 @Component({
   selector: 'ms-profile-page',
@@ -96,6 +98,37 @@ export class ProfilePage {
       const file = event.target.files[0];
       // Perform any action you want with the file, such as uploading it to a server
       alert(file.name);
+      const storage = getStorage();
+      const storageRef = ref(storage, file.name);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on('state_changed',
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+          }
+        },
+        (error) => {
+          // Handle unsuccessful uploads
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL);
+            this.profileImage=downloadURL;
+          });
+        }
+      );
     }
   }
 
@@ -117,11 +150,14 @@ export class ProfilePage {
   showPetsTick = false;
   showSpaceTick = false;
 
+
+
   //Save changes
+  profileImage: string = "https://picsum.photos/200/400?random=13";
   currentCell!: string;
   async saveChanges() {
     if (this.changeMade) {
-      
+      this.getEmailValue();
       let aboutMeToSend = this.aboutMeText;
       let majorToSend = this.majorText;
       let phoneToSend = this.phoneText;
@@ -161,9 +197,14 @@ export class ProfilePage {
       if (this.showSpaceTick)
         hobbies.push("space");
 
-      this.store.dispatch(new SaveProfileChanges(aboutMeToSend, majorToSend, phoneToSend,hobbies));
+      this.store.dispatch(new SaveProfileChanges(aboutMeToSend, majorToSend, phoneToSend, hobbies));
 
     }
+  }
+
+  @ViewChild('currentEmail') currentEmail?: ElementRef;
+  getEmailValue() {
+    alert(this.currentEmail?.nativeElement.innerText);
   }
 
   //Logout
